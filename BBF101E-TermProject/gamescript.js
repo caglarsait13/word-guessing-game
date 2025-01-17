b09b8775ea95fb11d9113a2e641436ab1bb30d95
itu-itis24-caglarm23 <caglarm23@itu.edu.tr>
2025-01-17 23:33:28 +0300

    game is uploaded

---
+++ BBF101E-TermProject/gamescript.js
@@ -0,0 +1,110 @@
+const b_letter = document.getElementById("b_letter");
+const l_letter = document.getElementById("l_letter");
+const i_letter = document.getElementById("i_letter");
+const n_letter = document.getElementById("n_letter");
+const k_letter = document.getElementById("k_letter");
+const submitBtn = document.getElementById("submit-btn");
+const resetBtn = document.getElementById("reset-btn");
+const heartsContain = document.getElementById("hearts");
+
+const letters = [b_letter, l_letter, i_letter, n_letter, k_letter];
+const myWord = "blink";
+let currentScore = 0;
+let lives = 3;
+let control = 0;
+
+resetBtn.classList.add("hidden");
+
+submitBtn.addEventListener("click", () => {
+    const input = document.getElementById("guess").value.toLowerCase();
+
+    if (input.length === 1) {
+        checkLetter(input);
+    } else if (input === myWord) {
+        showLetters();
+        currentScore = 100;
+        endGame(true);
+    } else {
+        lives = 0;
+        currentScore = 0;
+        changeHeartsDisplay(); 
+        endGame(false);
+    }
+    resetBtn.classList.remove("hidden");
+    restartGameboard();
+});
+
+resetBtn.addEventListener("click", resetGame);
+
+function checkLetter(letter) {
+    if (myWord[control] === letter) {
+        letters[control].classList.remove("hidden");
+        letters[control].parentElement.style.backgroundColor = "transparent"; 
+        currentScore += 20;
+        document.getElementById("guess").value = "";
+        control++;
+    } else {
+        lives -= 1;
+        document.getElementById("guess").value = "";
+        changeHeartsDisplay(); 
+    }
+
+    if (currentScore === 100) {
+        endGame(true);
+    } else if (lives === 0) {
+        endGame(false);
+    }
+}
+
+function showLetters() {
+    for (let i = 0; i < letters.length; i++) {
+        let charElement = letters[i];
+        charElement.classList.remove("hidden");
+        charElement.parentElement.style.backgroundColor = "transparent";
+    }
+}
+
+function endGame(istrue) {
+    setTimeout(() => {
+        alert(istrue ? "You won!" : "You lose!");
+    }, 100);
+
+    submitBtn.classList.add("hidden");
+    resetBtn.classList.remove("hidden");
+}
+
+function resetGame() {
+    currentScore = 0;
+    lives = 3;
+    control = 0;
+
+    for (let i = 0; i < letters.length; i++) {
+        let charElement = letters[i];
+        charElement.classList.add("hidden");
+        charElement.parentElement.style.backgroundColor = "#9bcee0";
+    }
+    submitBtn.classList.remove("hidden");
+    resetBtn.classList.add("hidden");
+
+    restartGameboard();
+}
+
+function restartGameboard() {
+    document.getElementById("score").innerText = `Points: ${currentScore}`;
+    changeHeartsDisplay(); 
+    document.getElementById("guess").value = "";
+    document.getElementById("guess").focus();
+}
+
+function changeHeartsDisplay() {
+    heartsContain.innerHTML = "";
+
+    for (let i = 0; i < lives; i++) {
+        const heartIcon = document.createElement("img");
+        heartIcon.src = "hearts.svg";
+        heartIcon.classList.add("hearts-icon");
+        heartsContain.appendChild(heartIcon);
+    }
+}
+
+restartGameboard();
