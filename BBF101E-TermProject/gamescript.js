fc972f73fa04676da782bf67b6ae47df17e8d528
itu-itis24-caglarm23 <caglarm23@itu.edu.tr>
2025-01-19 23:34:05 +0300

    User can submit any correct letter(uptaded)

--- BBF101E-TermProject/gamescript.js
+++ BBF101E-TermProject/gamescript.js
@@ -12,7 +12,7 @@ const letters = [b_letter, l_letter, i_letter, n_letter, k_letter];
 const myWord = "blink";
 let currentScore = 0;
 let lives = 3;
-let control = 0;
+let submittedLetters = ["","","","",""];
 
 /* reset and submit buttons are here*/
 resetBtn.classList.add("hidden");
@@ -39,14 +39,43 @@ resetBtn.addEventListener("click", resetGame);
 
 //This function checks the letters.
 function checkLetter(letter) {
-    if (myWord[control] === letter) {
-        letters[control].classList.remove("hidden");
-        letters[control].parentElement.style.backgroundColor = "transparent";
-        letters[control].parentElement.style.border = "none";
+    if (myWord[0] === letter && submittedLetters[0] != "b") {
+        letters[0].classList.remove("hidden");
+        letters[0].parentElement.style.backgroundColor = "transparent";
+        letters[0].parentElement.style.border = "none";
         currentScore += 20;
         document.getElementById("guess").value = "";
-        control++;
-    } else {
+        submittedLetters[0] = "b";
+    }else if (myWord[1] === letter && submittedLetters[1] != "l") {
+        letters[1].classList.remove("hidden");
+        letters[1].parentElement.style.backgroundColor = "transparent";
+        letters[1].parentElement.style.border = "none";
+        currentScore += 20;
+        document.getElementById("guess").value = "";
+        submittedLetters[1] = "l";
+    }else if (myWord[2] === letter && submittedLetters[2] != "i") {
+        letters[2].classList.remove("hidden");
+        letters[2].parentElement.style.backgroundColor = "transparent";
+        letters[2].parentElement.style.border = "none";
+        currentScore += 20;
+        document.getElementById("guess").value = "";
+        submittedLetters[3] = "i";
+    }else if (myWord[3] === letter && submittedLetters[3] != "n" ) {
+        letters[3].classList.remove("hidden");
+        letters[3].parentElement.style.backgroundColor = "transparent";
+        letters[3].parentElement.style.border = "none";
+        currentScore += 20;
+        document.getElementById("guess").value = "";
+        submittedLetters[4] = "n";
+    }else if (myWord[4] === letter && submittedLetters[4] != "k") {
+        letters[4].classList.remove("hidden");
+        letters[4].parentElement.style.backgroundColor = "transparent";
+        letters[4].parentElement.style.border = "none";
+        currentScore += 20;
+        document.getElementById("guess").value = "";
+        submittedLetters[5] = "k";
+    }
+    else {
         lives -= 1;
         document.getElementById("guess").value = "";
         changeHeartsDisplay(); 
@@ -80,7 +109,6 @@ function endGame(isTrue) {
 function resetGame() {
     currentScore = 0;
     lives = 3;
-    control = 0;
 
     for (let i = 0; i < letters.length; i++) {
         letters[i].classList.add("hidden");
